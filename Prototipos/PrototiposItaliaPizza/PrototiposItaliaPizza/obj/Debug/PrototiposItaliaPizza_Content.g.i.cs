64c6fccd647b05ac05cb6b81f126a80410ef5f81
gaby2327 <gabriela.uv@outlook.com>
2020-04-06 18:56:33 -0500

    Creación prototipo CU022 - Pedidos
    Se creó el prototipo del CU022 Pedidos

--- Prototipos/PrototiposItaliaPizza/PrototiposItaliaPizza/obj/Debug/PrototiposItaliaPizza_Content.g.i.cs
+++ Prototipos/PrototiposItaliaPizza/PrototiposItaliaPizza/obj/Debug/PrototiposItaliaPizza_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("logoitaliapizza.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("orden.png")]
 
 
