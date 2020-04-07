382e78f60e507ab1053efc605d2428269d7ec876
gaby2327 <gabriela.uv@outlook.com>
2020-04-07 12:41:33 -0500

    Creación de prototipo CU002 - Empleados
    Se creó el prototipo del CU002 - Empleados

--- Prototipos/PrototiposItaliaPizza/PrototiposItaliaPizza/obj/Debug/PrototiposItaliaPizza_Content.g.i.cs
+++ Prototipos/PrototiposItaliaPizza/PrototiposItaliaPizza/obj/Debug/PrototiposItaliaPizza_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("orden.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("buscar.png")]
 
 
