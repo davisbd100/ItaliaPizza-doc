3d03d525dcdc394d565ef898987bc5747ce832db
gaby2327 <gabriela.uv@outlook.com>
2020-04-12 13:22:46 -0500

    Creación de prototipo CU022 - Call Center
    Se creó la ventana para call center

--- Prototipos/PrototiposItaliaPizza/PrototiposItaliaPizza/obj/Debug/PrototiposItaliaPizza_Content.g.i.cs
+++ Prototipos/PrototiposItaliaPizza/PrototiposItaliaPizza/obj/Debug/PrototiposItaliaPizza_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("mas.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("cocina.png")]
 
 
