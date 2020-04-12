8324a7a43c10f9b26d1cacc51197dc9a2a061050
gaby2327 <gabriela.uv@outlook.com>
2020-04-11 21:08:41 -0500

    Modificación prototipo CU022 Pedidos
    Se cambió el orden de la interfaz

--- Prototipos/PrototiposItaliaPizza/PrototiposItaliaPizza/obj/Debug/PrototiposItaliaPizza_Content.g.i.cs
+++ Prototipos/PrototiposItaliaPizza/PrototiposItaliaPizza/obj/Debug/PrototiposItaliaPizza_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("fondoiniciopizza%20-%20copia.jpg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("pedido.png")]
 
 
