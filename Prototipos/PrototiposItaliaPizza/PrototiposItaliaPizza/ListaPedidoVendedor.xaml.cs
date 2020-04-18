64d3f36425ab8479996099932dd3cf1d36b762bb
davisbd100 <davisbd100@gmail.com>
2020-04-18 18:58:38 -0500

    Prototipo de CU025

---
+++ Prototipos/PrototiposItaliaPizza/PrototiposItaliaPizza/ListaPedidoVendedor.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PrototiposItaliaPizza
+{
+    /// <summary>
+    /// Lógica de interacción para Pedido.xaml
+    /// </summary>
+    public partial class ListaPedidoVendedor : Window
+    {
+        public ListaPedidoVendedor()
+        {
+            InitializeComponent();
+        }
+    }
+}
